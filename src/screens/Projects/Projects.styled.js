f6e99d3a40c404fc3a9626225e3678dce6edd5ef
stephanLeece <stephan.leece@outlook.com>
2019-09-13 23:10:13 +0200

    margin tweaks

--- src/screens/Projects/Projects.styled.js
+++ src/screens/Projects/Projects.styled.js
@@ -4,11 +4,14 @@ import ProjectComponent from "./Project";
 import GridWrap from "../../common/GridWrap";
 
 const Grid = styled(GridWrap)`
+  margin: 0;
 `;
 
 const Project = styled(ProjectComponent)`
   grid-column: 1 / -1;
   margin-bottom: 32px;
+  padding-left: 8px;
+  border: 1px dashed red;
 `;
 
 export { Grid, Project };
