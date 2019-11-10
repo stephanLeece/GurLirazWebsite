962fb2c3575cafc804d4df77ed1e3623607e33d3
stephanLeece <stephan.leece@outlook.com>
2019-11-10 17:42:30 +0100

    move site link above videos in projects

--- src/screens/Projects/Project/Project.jsx
+++ src/screens/Projects/Project/Project.jsx
@@ -18,15 +18,15 @@ const Project = ({
     <Wrap className={className}>
       {title && <Title>{title}</Title>}
       {description && <Description>{description}</Description>}
-      <Carousel>
-        {videos &&
-          videos.map(video => <Video youtubeId={video.youtubeVideoId} />)}
-      </Carousel>
       {projectWebsite && (
         <Link href={projectWebsite} target="_blank" rel="noopener noreferrer">
           <LinkText>Visit Site</LinkText>
         </Link>
       )}
+      <Carousel>
+        {videos &&
+          videos.map(video => <Video youtubeId={video.youtubeVideoId} />)}
+      </Carousel>
     </Wrap>
   );
 };
