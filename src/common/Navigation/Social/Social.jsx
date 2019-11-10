2d6c11b19cdb6532bb8c91200ef69c33e139a6d2
stephanLeece <stephan.leece@outlook.com>
2019-11-10 16:34:23 +0100

    add norefer to external links

--- src/common/Navigation/Social/Social.jsx
+++ src/common/Navigation/Social/Social.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Wrap, Img } from "./Social.styled";
 
 const SocialLink = ({className, linkData}) => (
-  <Wrap className={className} href={linkData.linkUrl} target="_blank">
+  <Wrap className={className} href={linkData.linkUrl} target="_blank" rel="noopener noreferrer">
     <Img src={linkData.linkImage.file.url} alt={linkData.linkName}/>
   </Wrap>
 );
