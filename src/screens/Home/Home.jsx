e09be9f1c38926ec85f1bf9e7123515b48fce095
stephanLeeceGrover <stephanl@grover.com>
2021-11-29 17:57:02 +0100

    remove soundCloud playr

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,40 +1,23 @@
 import React from "react";
 import Markdown from "markdown-to-jsx";
 
-import {
-  Wrap,
-  Title,
-  TextBlock,
-  Text,
-  Image,
-  SoundCloudWrap
-} from "./Home.styled";
+import { Wrap, Title, TextBlock, Text, Image } from "./Home.styled";
 
-const Home = ({ homeScreenData: {
-  contentBlocks
-}, imageUrls }) => (
+const Home = ({ homeScreenData: { contentBlocks }, imageUrls }) => (
   <Wrap>
     <Title>Gur Liraz</Title>
-    {contentBlocks && contentBlocks.map(contentBlock => (
-    <TextBlock>
-    <Text>
-      <Markdown>
-        {contentBlock.text.childMarkdownRemark.html}
-      </Markdown>
-    </Text>
-    <Image src={contentBlock.image.file.url} alt={contentBlock.image.title} />
-  </TextBlock>
-    ))}
-    <SoundCloudWrap>
-      <iframe
-        width="100%"
-        height="300"
-        scrolling="no"
-        frameborder="no"
-        allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/859737146&color=%23c0c0c0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-      ></iframe>
-    </SoundCloudWrap>
+    {contentBlocks &&
+      contentBlocks.map((contentBlock) => (
+        <TextBlock>
+          <Text>
+            <Markdown>{contentBlock.text.childMarkdownRemark.html}</Markdown>
+          </Text>
+          <Image
+            src={contentBlock.image.file.url}
+            alt={contentBlock.image.title}
+          />
+        </TextBlock>
+      ))}
   </Wrap>
 );
 
--- src/screens/Home/Home.styled.js
+++ src/screens/Home/Home.styled.js
@@ -70,10 +70,4 @@ const Image = styled.img`
   }
 `;
 
-const SoundCloudWrap = styled.div`
-  @media ${BREAK_POINTS.desktopMin} {
-    width: 25%;
-  }
-`;
-
-export { Wrap, Title, TextBlock, Text, Image, SoundCloudWrap };
+export { Wrap, Title, TextBlock, Text, Image };
