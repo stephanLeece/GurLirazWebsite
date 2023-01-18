671ac8867e52d9e5fc05bbcc47147b9a2d18c0be
Stephan Leece <stephan.leece@contentful.com>
2023-01-18 18:56:56 +0100

    remove recompose

--- src/screens/Shows/Shows.jsx
+++ src/screens/Shows/Shows.jsx
@@ -1,19 +1,58 @@
 import React from "react";
 
-import { Wrap, ShowListSection, ShowList, ImageSection, Image } from "./Shows.styled";
+import {
+  Wrap,
+  ShowListSection,
+  ShowList,
+  ImageSection,
+  Image
+} from "./Shows.styled";
 
-const Shows = ({ upcomingShows, pastShows, imageUrls }) => (
-  <Wrap>
-    <ShowListSection>
-      <ShowList title="Upcoming Shows" shows={upcomingShows.reverse()} />
-      <ShowList title="Past Shows" shows={pastShows} />
-    </ShowListSection>
-    <ImageSection>
-      {imageUrls.map(image => (
-        <Image src={image.url} alt={image.title} />
-      ))}
-    </ImageSection>
-  </Wrap>
-);
+import { dateHasPassed } from "../../utils/time";
+
+const getShowDetails = ({ rawShows: { edges } }) => {
+  const upcomingShows = [];
+  const pastShows = [];
+  for (var i = 0; i < edges.length; i++) {
+    if (dateHasPassed(edges[i].node.date)) {
+      pastShows.push(edges[i].node);
+    } else {
+      upcomingShows.push(edges[i].node);
+    }
+  }
+  return {
+    upcomingShows,
+    pastShows
+  };
+};
+
+const getImageUrls = ({ photos }) => {
+  return {
+    imageUrls: photos.map((photo) => {
+      return {
+        url: photo.file.url,
+        title: photo.title
+      };
+    })
+  };
+};
+
+const Shows = ({ rawShows, photos }) => {
+  const { upcomingShows, pastShows } = getShowDetails({ rawShows });
+  const { imageUrls } = getImageUrls({ photos });
+  return (
+    <Wrap>
+      <ShowListSection>
+        <ShowList title="Upcoming Shows" shows={upcomingShows.reverse()} />
+        <ShowList title="Past Shows" shows={pastShows} />
+      </ShowListSection>
+      <ImageSection>
+        {imageUrls.map((image) => (
+          <Image src={image.url} alt={image.title} />
+        ))}
+      </ImageSection>
+    </Wrap>
+  );
+};
 
 export default Shows;
--- src/screens/Shows/index.js
+++ src/screens/Shows/index.js
@@ -1,32 +1,3 @@
-import { compose, withProps } from "recompose";
-import { dateHasPassed } from "../../utils/time";
-
 import Shows from "./Shows";
 
-const enhance = compose(
-  withProps(({ rawShows: { edges } }) => {
-    const upcomingShows = [];
-    const pastShows = [];
-    for (var i = 0; i < edges.length; i++) {
-      if (dateHasPassed(edges[i].node.date)) {
-        pastShows.push(edges[i].node);
-      } else {
-        upcomingShows.push(edges[i].node);
-      }
-    }
-    return {
-      upcomingShows,
-      pastShows
-    };
-  }),
-  withProps(({ photos }) => {
-    return {
-      imageUrls: photos.map(photo => { return {
-        url: photo.file.url,
-        title: photo.title,
-      }})
-    };
-  })
-);
-
-export default enhance(Shows);
+export default Shows;
